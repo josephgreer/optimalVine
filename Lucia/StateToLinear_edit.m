94d344f5279e9acc8b832104c5693fd13455eeaf
ganlucia <ganlucia@users.noreply.github.com>
2017-06-06 15:00:22 -0700

    Delete StateToLinear_edit.m

--- Lucia/StateToLinear_edit.m
+++
@@ -1,5 +0,0 @@
-function state = StateToLinear_edit(params, state)
-
-[gridX, gridY] = GenerateGrid(params);
-[destX, destY] = findClosestPoint(params, state);
-state = intersect(find(gridX==destX), find(gridY==destY));
